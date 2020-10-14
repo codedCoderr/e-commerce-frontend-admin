f1a33c9ec5f5a22a5bedc485933dc5c11cc1b19a
Busola Olowu <bolowu@parthianpartnersng.com>
2020-10-14 16:46:56 +0100

    initial commit

---
+++ client-admin/src/containers/Login/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Layout from "../../components/Layout";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import Input from "../../components/UI/Input";
+
+const Login = () => {
+  return (
+    <Layout>
+      <Container>
+        <Row style={{ marginTop: "50px" }}>
+          <Col md={{ span: 6, offset: 3 }}>
+            <Form>
+              <Input
+                label="Email"
+                placeholder="Email"
+                value=""
+                type="email"
+                onChange={() => {}}
+              />
+
+              <Input
+                label="Password"
+                placeholder="Password"
+                value=""
+                type="text"
+                onChange={() => {}}
+              />
+
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </Layout>
+  );
+};
+
+export default Login;
